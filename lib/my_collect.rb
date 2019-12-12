4aada632cfe27250a5bea03ead6268fada7bc985
Alex Engelhart <asengelhart@yahoo.com>
2019-12-12 16:51:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  if !block_given?
+    return array
+  end 
+  result = []
+  count = 0 
+  while count < array.size do 
+    result[count] = yield(array[count])
+    count += 1 
+  end 
+  result
+end 
